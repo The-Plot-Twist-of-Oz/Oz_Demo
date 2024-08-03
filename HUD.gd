73f4be9f20e5c1121788046ddfeba274beaaaf44
markusbkk <happyferret2022@gmail.com>
2024-08-03 09:53:30 +0200

    Initial commit
    Basic setup with "programmer art", some TODOs and fairly simple
    code based on an existing example.
    
    Will do the artwork and implement the green orb code today.

---
+++ HUD.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+signal start_game
+
+
+func show_message(text):
+	$GameTitle.text = text
+	$GameTitle.show()
+	$MessageTimer.start()
+
+
+func show_game_over():
+	show_message("Game Over")
+	yield($MessageTimer, "timeout")
+	$GameTitle.text = "Dodge the\nCreeps"
+	$GameTitle.show()
+	yield(get_tree().create_timer(1), "timeout")
+	$StartButton.show()
+
+
+func update_score(score):
+	$ScoreMeter.text = str(score)
+
+func update_health(health):
+	$HealthMeter.text = str(health)
+
+func _on_StartButton_pressed():
+	$StartButton.hide()
+	emit_signal("start_game")
+
+
+func _on_MessageTimer_timeout():
+	$GameTitle.hide()
