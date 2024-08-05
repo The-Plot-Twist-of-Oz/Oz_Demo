695d93e26fb39ac456edabac1e10bf63c48169d9
RuinedKingg <pozdnyakov_k_v@mail.ru>
2024-08-05 19:03:09 +0300

    Made toxic orbs mechanics, grass slownes and some other improvements

--- Scenes/Main/Main.gd
+++ Scenes/Main/Main.gd
@@ -2,25 +2,32 @@ extends Node
 
 var score
 var health
+export var toxic_orb: PackedScene
 
 func _ready():
 	randomize()
 
 
 func game_over():
+	get_tree().call_group("mobs", "queue_free")
+	get_tree().call_group("orbs", "queue_free")
+	$OrbTimer.stop()
+	$MobPath1/Timer.stop()
+	$MobPath2/Timer.stop()
 	$ScoreTimer.stop()
 	$HUD.show_game_over()
+	$Player.hide()
 	$Music.stop()
 	$DeathSound.play()
 
 func new_game():
-	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	health = 10
 	$MobPath1/Timer.start()
 	$MobPath2/Timer.start()
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$OrbTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	$Music.play()
@@ -39,17 +46,28 @@ func _on_Player_hit():
 	$HUD.update_health(health)
 	if health < 0:
 		$HUD.update_health("Game Over")
+		game_over()
 
 
 func _on_OrbTimer_timeout():
-	pass # Replace with function body.
+	var orb = toxic_orb.instance()
+	
+	orb.position = Vector2(rand_range(192, 1728), rand_range(192, 888))
+	
+	add_child(orb)
 
 
-func _on_Area2D_body_exited(body):
-	if body.name == "Player":
-		body.speed -= 100
+func _on_Area2D_area_entered(area):
+	if area.name == "Player":
+		area.speed += 150
 
 
-func _on_Area2D_body_entered(body):
-	if body.name == "Player":
-		body.speed += 100
+func _on_Area2D_area_exited(area):
+	if area.name == "Player":
+		area.speed -= 150
+
+
+func _on_Player_heal():
+	if health < 10:
+		health += 1
+		$HUD.update_health(health)
