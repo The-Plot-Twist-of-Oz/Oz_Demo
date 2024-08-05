c25ee8be6600143386541e57e96b15c0de8184e9
RuinedKingg <pozdnyakov_k_v@mail.ru>
2024-08-05 11:38:03 +0300

    Add damage mechanics

--- Scenes/Player/Axe/Area2D.gd
+++ Scenes/Player/Axe/Area2D.gd
@@ -1,21 +1,9 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+export var damage = 75
 
 func _on_Area2D_body_entered(body):
-	if body.name != "Player":
-		body.queue_free()
+	var mobs = get_tree().get_nodes_in_group("mobs")
+	
+	if mobs.bsearch(body) > 0:
+		body.emit_signal("hit", damage)
